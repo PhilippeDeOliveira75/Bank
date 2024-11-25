ee834b598c2552e1fe1fd19ce80c0a9e9ab0e25d
Philippe De Oliveira <philippe.deoliveira75@gmail.com>
2024-11-25 10:33:23 +0100

    first commit

---
+++ src/pages/Layout/layout.jsx
@@ -0,0 +1,20 @@
+import { Outlet } from 'react-router'
+
+import { Header, Footer } from '@components/index.jsx'
+
+
+function Layout  ()  {
+
+    return (
+
+        <div className='Layout'>
+            <Header />
+            <Outlet />
+            <Footer />
+        </div>
+
+    )
+
+}
+
+export default Layout
